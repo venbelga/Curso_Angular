9dfd39d47fa6b724f59ac1267a13b9756b0f7f80
venbelga <venanciobelga1@outlook.com>
2026-05-25 12:06:40 -0300

    Adiciona component de calculadora

--- conceitos-basicos/src/app/app.ts
+++ conceitos-basicos/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Calculadora } from './calculadora/calculadora';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('conceitos-basicos');
+  protected readonly calculadora = new Calculadora();
 }
